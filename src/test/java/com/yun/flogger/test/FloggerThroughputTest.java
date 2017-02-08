2524e25fa2ebcb4588cc98253c59cc055c8ef8ad
yunfengCheng <869827095@qq.com>
2017-02-08 17:41:18 +0800

    FLogger吞吐量测试代码

--- src/test/java/com/yun/flogger/test/FloggerThroughputTest.java
+++ src/test/java/com/yun/flogger/test/FloggerThroughputTest.java
@@ -1,6 +1,7 @@
 package com.yun.flogger.test;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import com.cyfonly.flogger.FLogger;
 
@@ -15,13 +16,13 @@ public class FloggerThroughputTest {
 	
 	private static String record_100_byte = "Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.";   //100字节
 	private static String record_200_byte = "Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.";   //200字节
-	private static String record_400_byte = "Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.";   //200字节
-	private static int messageCount = 1000000;
+	private static String record_400_byte = "Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.Performance Testing.";   //400字节
+	private static AtomicInteger messageCount = new AtomicInteger(0);
+	private static int count = 1000000;  //基准数值，以messageCount为准
 	private static int threadNum = 1;  //1,2,4,8,16,32
 	
 	public static void main(String[] args) throws InterruptedException{
 		
-		final int singleCount = messageCount / threadNum;
 		final CountDownLatch latch = new CountDownLatch(threadNum);
 		
 		long st = System.currentTimeMillis();
@@ -29,8 +30,9 @@ public class FloggerThroughputTest {
 			new Thread(new Runnable() {
 				@Override
 				public void run() {
-					for(int j=0; j<singleCount; j++){
+					while(messageCount.get() < count){
 						flogger.info(record_400_byte);
+						messageCount.incrementAndGet();
 					}
 					latch.countDown();
 				}
@@ -39,7 +41,7 @@ public class FloggerThroughputTest {
 		latch.await();
 		long et = System.currentTimeMillis();
 		
-		System.out.println("messageCount=" + messageCount + ", threadNum=" + threadNum + ", costTime=" + (et-st) +"ms, throughput=" + (1*1000*messageCount/(et-st)));
+		System.out.println("messageCount=" + messageCount.get() + ", threadNum=" + threadNum + ", costTime=" + (et-st) +"ms, throughput=" + (1*1000*messageCount.get()/(et-st)));
 		System.exit(0);
 	}
 
